c7cafbe6a023780aba435293ebd0569ccec3655e
vykunta <mudragalla96@gmail.com>
2022-09-16 23:13:52 +0530

    special variables

--- 04-special-variables.sh
+++ 04-special-variables.sh
@@ -1,7 +1,7 @@
-echo $0
-echo $1
-echo $2
-echo $*
-echo $@
-echo $#
+echo $0 # script name
+echo $1 # first argument
+echo $2 # second argument
+echo $* # all arguments
+echo $@ # all arguments
+echo $# # no of arguments
 
